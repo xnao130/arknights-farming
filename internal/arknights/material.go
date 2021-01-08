9b0ef28855b825b569d7bd967dfc8ff007f8ea08
xnao130 <xnao130@gmail.com>
2021-01-08 13:42:07 +0900

    Add materials

--- internal/arknights/material.go
+++ internal/arknights/material.go
@@ -5,18 +5,48 @@ type material string
 const (
 	// Polyester is "初級エステル".
 	Polyester material = "Polyester"
+	// PolyesterLump is "上級エステル".
+	PolyesterLump material = "Polyester Lump"
 	// Sugar is "初級糖原".
 	Sugar material = "Sugar"
+	// SugarPack is "中級糖原".
+	SugarPack material = "Sugar Pack"
+	// Oriron is "初級異鉄".
+	Oriron material = "Oriron"
+	// OrironCluster is "中級異鉄".
+	OrironCluster material = "Oriron Cluster"
 	// OrironBlock is "上級異鉄".
 	OrironBlock material = "Oriron Block"
 	// Device is "初級装置".
 	Device material = "Device"
+	// OptimizedDevice is "上級装置".
+	OptimizedDevice material = "Optimized Device"
 	// OrirockCube is "初級源岩".
 	OrirockCube material = "Orirock Cube"
+	// OrirockCluster is "中級源岩".
+	OrirockCluster material = "Orirock Cluster"
 	// OrirockConcentration is "上級源岩".
 	OrirockConcentration material = "Orirock Concentration"
+	// Polyketon is "初級アケトン".
+	Polyketon material = "Polyketon"
+	// Aketon is "中級アケトン".
+	Aketon material = "Aketon"
 	// KetonColloid is "上級アケトン".
 	KetonColloid material = "Keton Colloid"
+	// ManganeseTrihydrate is "上級マンガン".
+	ManganeseTrihydrate material = "Manganese Trihydrate"
+	// Grindstone is "砥石".
+	Grindstone material = "Grindstone"
+	// GrindstonePentahydrate is "上級砥石".
+	GrindstonePentahydrate material = "Grindstone Pentahydrate"
+	// LoxicKohl is "合成コール".
+	LoxicKohl material = "Loxic Kohl"
+	// WhiteHorseKohl is "上級合成コール".
+	WhiteHorseKohl material = "White Horse Kohl"
+	// RMA7012 is "RMA70-12".
+	RMA7012 material = "RMA70-12"
+	// IncandescentAlloyBlock is "上級熾合金".
+	IncandescentAlloyBlock material = "Incandescent Alloy Block"
 	// BipolarNanoflake is "ナノフレーク".
 	BipolarNanoflake material = "Bipolar Nanoflake"
 	// D32Steel is "D32鋼".
--- internal/arknights/operator.go
+++ internal/arknights/operator.go
@@ -70,52 +70,148 @@ func find(name string) operator {
 		"Weedy": {
 			rarity: 6,
 			class:  Specialist,
+			elite1: Materials{
+				Device: 6,
+				Sugar:  4,
+			},
+			elite2: Materials{
+				D32Steel:            4,
+				ManganeseTrihydrate: 6,
+			},
 		},
 		// ★★★★★
 		"Amiya": {
 			rarity: 5,
 			class:  Caster,
+			elite1: Materials{
+				Device: 4,
+				Oriron: 4,
+			},
+			elite2: Materials{
+				OrirockConcentration: 10,
+				LoxicKohl:            10,
+			},
 		},
 		"Bibeak": {
 			rarity: 5,
 			class:  Guard,
+			elite1: Materials{
+				Oriron:      4,
+				OrirockCube: 3,
+			},
+			elite2: Materials{
+				ManganeseTrihydrate: 8,
+				RMA7012:             8,
+			},
 		},
 		"Elysium": {
 			rarity: 5,
 			class:  Vanguard,
+			elite1: Materials{
+				Polyester: 4,
+				Sugar:     3,
+			},
+			elite2: Materials{
+				IncandescentAlloyBlock: 7,
+				Aketon:                 16,
+			},
 		},
 		"Lappland": {
 			rarity: 5,
 			class:  Guard,
+			elite1: Materials{
+				Device:      3,
+				OrirockCube: 4,
+			},
+			elite2: Materials{
+				OptimizedDevice: 6,
+				OrironCluster:   10,
+			},
 		},
 		"Liskarm": {
 			rarity: 5,
 			class:  Defender,
+			elite1: Materials{
+				Sugar:       5,
+				OrirockCube: 3,
+			},
+			elite2: Materials{
+				GrindstonePentahydrate: 7,
+				Aketon:                 15,
+			},
 		},
 		"Ptilopsis": {
 			rarity: 5,
 			class:  Medic,
+			elite1: Materials{
+				OrirockCube: 8,
+				Sugar:       2,
+			},
+			elite2: Materials{
+				OrirockConcentration: 9,
+				Grindstone:           10,
+			},
 		},
 		"Specter": {
 			rarity: 5,
 			class:  Guard,
+			elite1: Materials{
+				OrirockCube: 6,
+				Polyketon:   3,
+			},
+			elite2: Materials{
+				WhiteHorseKohl: 8,
+				Aketon:         15,
+			},
 		},
 		"Texas": {
 			rarity: 5,
 			class:  Vanguard,
+			elite1: Materials{
+				Polyester: 5,
+				Oriron:    3,
+			},
+			elite2: Materials{
+				PolyesterLump:  8,
+				OrirockCluster: 16,
+			},
 		},
 		// ★★★★
 		"Dur-nar": {
 			rarity: 4,
 			class:  Defender,
+			elite1: Materials{
+				OrirockCube: 1,
+				Polyester:   1,
+			},
+			elite2: Materials{
+				OrirockCluster: 19,
+				RMA7012:        8,
+			},
 		},
 		"Ethan": {
 			rarity: 4,
 			class:  Specialist,
+			elite1: Materials{
+				Sugar:  1,
+				Oriron: 1,
+			},
+			elite2: Materials{
+				SugarPack:      17,
+				OrirockCluster: 14,
+			},
 		},
 		"Utage": {
 			rarity: 4,
 			class:  Guard,
+			elite1: Materials{
+				Device: 1,
+				Sugar:  1,
+			},
+			elite2: Materials{
+				Aketon:         14,
+				OrirockCluster: 14,
+			},
 		},
 	}
 	op, ok := m[name]
